8363ef98c77faf025adbf3eb016f4921511ac23e
Ismael Barboza <ismabarboza@gmail.com>
2024-08-19 19:08:11 -0300

    Adicionado dependências do encore do container e primeira tela

--- src/Config/bindings.php
+++ src/Config/bindings.php
@@ -11,7 +11,15 @@ use Isma\Datafrete\Modules\DistanceCalculator\Gateway\CepFinderInterface;
 use Isma\Datafrete\Modules\DistanceCalculator\Gateway\DistanceRepositoryInterface;
 use Psr\Container\ContainerInterface;
 use Slim\Views\Twig;
+use Symfony\Bridge\Twig\Extension\AssetExtension;
+use Symfony\Component\Asset\Package;
+use Symfony\Component\Asset\Packages;
+use Symfony\Component\Asset\VersionStrategy\JsonManifestVersionStrategy;
 use Symfony\Component\Cache\Adapter\RedisAdapter;
+use Symfony\WebpackEncoreBundle\Asset\EntrypointLookup;
+use Symfony\WebpackEncoreBundle\Asset\EntrypointLookupCollection;
+use Symfony\WebpackEncoreBundle\Asset\TagRenderer;
+use Symfony\WebpackEncoreBundle\Twig\EntryFilesTwigExtension;
 use function DI\create;
 
 return [
@@ -32,10 +40,21 @@ return [
     );
   },
   CepCacheInterface::class => fn(RedisAdapter $cache) => new CepDistance($cache),
-  Twig::class => function(Config $config) {
+  Twig::class => function (ContainerInterface $container) {
+    $config = $container->get(Config::class);
     $twig = Twig::create(TEMPLATES_PATH, [
       'auto_reload' => $config->get('app.env'),
+      'cache' => TEMPLATES_CACHE_PATH,
     ]);
+    $twig->addExtension(new EntryFilesTwigExtension($container));
+    $twig->addExtension(new AssetExtension($container->get('webpack_encore.packages')));
     return $twig;
-  }
+  },
+  'webpack_encore.packages' => fn() => new Packages(
+    new Package(new JsonManifestVersionStrategy(BUILD_PATH . '/manifest.json'))
+  ),
+  'webpack_encore.tag_renderer' => fn(ContainerInterface $container) => new TagRenderer(
+    new EntrypointLookup(BUILD_PATH . '/entrypoints.json'),
+    $container->get('webpack_encore.packages')
+  ),
 ];
--- src/Config/constants.php
+++ src/Config/constants.php
@@ -5,3 +5,4 @@ const TEMPLATES_PATH = __DIR__ . '/../../resources/templates';
 const TEMPLATES_CACHE_PATH = __DIR__ . '/../../storage/cache';
 const CONFIGS_PATH = __DIR__ . '/definitions.php';
 const CONTAINER_BINDINGS = __DIR__ . '/bindings.php';
+const BUILD_PATH = __DIR__ . '/../../public/build';
