21158fe7f957276b97c3214b0ba801ca9aa64cb8
Ismael Barboza <ismabarboza@gmail.com>
2024-08-20 17:37:03 -0300

    Adição de form para importar ceps

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,8 +3,13 @@ import "bootstrap";
 import DataTable from "datatables.net-dt";
 import languagePTBR from "datatables.net-plugins/i18n/pt-BR.mjs";
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   let table = initTable();
+
+  setTimeout(() => {
+    table.destroy();
+    table = initTable();
+  }, 1000 * 60);
   /**
    * @type {HTMLFormElement} form
    */
@@ -42,7 +47,7 @@ document.addEventListener('DOMContentLoaded', () => {
       searching: false,
       orderMulti: false,
       ordering: false,
-      ajax: '/list',
+      ajax: "/list",
       columns: [
         {
           data: "origin",
@@ -59,7 +64,7 @@ document.addEventListener('DOMContentLoaded', () => {
           searchable: false,
           orderable: false,
         },
-      ]
+      ],
     });
   }
 
@@ -68,4 +73,29 @@ document.addEventListener('DOMContentLoaded', () => {
     table = initTable();
   }
 
+  /**
+   * @type {HTMLFormElement}
+   */
+  const importarCepsForm = document.forms["importar-cep"];
+  importarCepsForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const formData = new FormData(importarCepsForm);
+    fetch("/import", {
+      method: "POST",
+      body: formData,
+      headers: {
+        encType: "multipart/form-data",
+      },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          alert("Erro ao realizar operação. Tente novamente");
+          return {};
+        }
+      })
+      .then(() => {
+        alert("Importado com sucesso!, Aguarde até todos os dados serem carregados");
+        reset();
+      });
+  });
 });
